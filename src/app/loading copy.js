99c3084b5f906c5ce57420b8bdb936b7bdae0822
mohammadmohammadi785-crypto <alisafamohammadi785@gmail.com>
2026-08-05 16:07:08 +0430

    Changes added

--- src/app/loading copy.js
+++
@@ -1,4 +0,0 @@
-export default function Loading() {
-	// You can add any UI inside Loading, including a Skeleton.
-	return <h6 className="loading-style">Loading...</h6>;
-}
--- src/app/loading.js
+++
@@ -1,6 +0,0 @@
-import LoadingComponent from "@/Components/Helpers/LoadingComponent";
-
-export default function Loading() {
-	// You can add any UI inside Loading, including a Skeleton.
-	//  <h6 className="loading-style">Loading...</h6>;
-return <LoadingComponent />;}
